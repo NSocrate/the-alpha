0da7530a099b06a2bb6cd3abb36681440389b94f
NSocrate <100295674+NSocrate@users.noreply.github.com>
2023-05-17 00:00:28 +0200

    mon projet

---
+++ assets/js/app.js
@@ -0,0 +1,64 @@
+$('#hero-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    dots:false,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#Projets-carousel').owlCarousel({
+    loop:true,
+    margin:35,
+    nav:false,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
+$('#Equipe-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
